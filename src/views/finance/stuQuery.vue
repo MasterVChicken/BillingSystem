006db7b7e37d07d7ecad6bda1e72c0e527941129
MasterVChicken <2514763639@qq.com>
2023-05-12 12:22:39 +0800

    feat: 修复缴费bug

--- src/views/finance/stuQuery.vue
+++ src/views/finance/stuQuery.vue
@@ -11,9 +11,10 @@
     </el-select>
     <el-button @click="search">查询</el-button>
     <div style="margin-bottom: 20px;margin-top: 20px; align-items: center; width: 100%">年度总体费用</div>
-    <el-table size="small" :data="basicData" style="width: 100%;" border :span-method="basicSpan"
-              :summary-method="getSummaries" show-summary>
+    <el-table size="small" :data="basicData" style="width: 100%;" border :span-method="basicSpan">
       <el-table-column align="center" prop="year" label="学年">
+      </el-table-column>
+      <el-table-column align="center" prop="name" label="姓名">
       </el-table-column>
       <el-table-column align="center" prop="type" label="类型">
       </el-table-column>
@@ -23,7 +24,7 @@
       </el-table-column>
       <el-table-column align="center" label="操作">
         <template slot-scope="scope">
-          <el-button @click="totalPay(scope.row)" :disabled="scope.row.is_payed === 0 ? false:true">缴纳</el-button>
+          <el-button @click="totalPay(scope.row)" :disabled="scope.row.is_payed === 1 ? true:false">缴纳</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -37,7 +38,7 @@
       <el-table-column align="center" prop="flag" label="缴费状态" :formatter="flagFormatter"></el-table-column>
       <el-table-column align="center" label="缴费操作">
         <template slot-scope="scope">
-          <el-button @click="otherPay(scope.row)" :disabled="scope.row.flag === 0 ? true:false">缴纳</el-button>
+          <el-button @click="otherPay(scope.row)" :disabled="scope.row.flag === 1 ? false : true">缴纳</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -55,14 +56,7 @@ export default {
       year: '',
       s_no: '',
       basicData: [],
-      otherList: [{
-        s_name: '',
-        s_no: '',
-        year: '',
-        remark: '',
-        fee: '',
-        flag: ''
-      }]
+      otherList: []
     }
   },
   methods: {
@@ -85,20 +79,23 @@ export default {
         for (let i = 0; i < response.data.length; i++) {
           this.basicData.push({
             'year': response.data[i].year,
+            'name': response.data[i].s_name,
             'type': '学费',
             'money': response.data[i].xuefei,
             'flag': response.data[i].xuefeiflag,
-            'is_payed': response.data[i].xuefeiflag === 0 ? 1 : 0
+            'is_payed': response.data[i].xuefeiflag === 1 ? 0 : 1
           })
           this.basicData.push({
             'year': response.data[i].year,
+            'name': response.data[i].s_name,
             'type': '住宿费',
             'money': response.data[i].zhusufei,
             'flag': response.data[i].zhusufeiflag,
-            'is_payed': response.data[i].zhusufeiflag === 0 ? 1 : 0
+            'is_payed': response.data[i].zhusufeiflag === 1 ? 0 : 1
           })
           this.basicData.push({
             'year': response.data[i].year,
+            'name': response.data[i].s_name,
             'type': '其他费用',
             'money': response.data[i].qitatotal,
             'flag': response.data[i].qitaflag,
@@ -134,37 +131,19 @@ export default {
           }
         }
       }
-    },
-    getSummaries(param) {
-      const {columns, data} = param
-      const sums = []
-      columns.forEach((column, index) => {
-        if (index === 0) {
-          sums[index] = '应缴';
-          return;
-        }
-        if (index === 2) {
-          sums[index] = '未缴';
-          return;
-        }
-        if (index === 1) {
-          sums[1] = 0
-          for (let i = 0; i < param.data.length; i++) {
-            sums[1] += Number(param.data[i].money)
+      if (columnIndex === 1) {
+        if (rowIndex % 3 === 0) {
+          return {
+            rowspan: 3,
+            colspan: 1
           }
-          sums[1] += '元'
-        }
-        if (index === 3) {
-          sums[3] = 0
-          for (let i = 0; i < param.data.length; i++) {
-            if (param.data[i].flag === 1) {
-              sums[3] += param.data[i].money
-            }
+        } else if (rowIndex % 3 === 1 || rowIndex % 3 === 2) {
+          return {
+            rowspan: 0,
+            colspan: 0
           }
-          sums[3] += '元'
         }
-      });
-      return sums;
+      }
     },
     getOtherPay() {
       let params = new URLSearchParams()
@@ -180,25 +159,26 @@ export default {
       })
     },
     totalPay(data){
-      let params = new URLSearchParams()
-      let f_id = (data.type==='学费'?'1':'2')
-      params.append('S_no',this.s_no)
-      params.append('F_id',f_id)
-      params.append('remark',data.type)
-      params.append('money',data.money)
-      params.append('year',data.year)
-      axios.post('/treasurer/jiaofei', params, {
-        headers: {
-          'Access-Control-Allow-Credentials': 'true', //解决session问题
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' //将表单数据传递转化为form-data类型
-        }
-      }).then((response)=>{
-        if(response.data===1){
-          this.$message.success('缴费成功')
-        }else{
-          this.$message.error('缴费失败')
-        }
-      })
+      console.log(data.flag)
+      // let params = new URLSearchParams()
+      // let f_id = (data.type==='学费'?'1':'2')
+      // params.append('S_no',this.s_no)
+      // params.append('F_id',f_id)
+      // params.append('remark',data.type)
+      // params.append('money',data.money)
+      // params.append('year',data.year)
+      // axios.post('/treasurer/jiaofei', params, {
+      //   headers: {
+      //     'Access-Control-Allow-Credentials': 'true', //解决session问题
+      //     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' //将表单数据传递转化为form-data类型
+      //   }
+      // }).then((response)=>{
+      //   if(response.data===1){
+      //     this.$message.success('缴费成功')
+      //   }else{
+      //     this.$message.error('缴费失败')
+      //   }
+      // })
     },
     otherPay(){
       let params = new URLSearchParams()
@@ -211,6 +191,12 @@ export default {
         headers: {
           'Access-Control-Allow-Credentials': 'true', //解决session问题
           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' //将表单数据传递转化为form-data类型
+        }
+      }).then((response)=>{
+        if(response.data===1){
+          this.$message.success('缴费成功')
+        }else{
+          this.$message.error('缴费失败')
         }
       })
     }
